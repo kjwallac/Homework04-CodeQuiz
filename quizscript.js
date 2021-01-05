daeccac09b1eb9a095271fc8eeef2c91bf7e6e94
kjwallac <kristinajwallace@gmail.com>
2021-01-04 23:50:50 -0800

    created and styled 60 second timer

--- quizscript.js
+++ quizscript.js
@@ -106,4 +106,25 @@ choices.forEach(choice => {
     });
 });
 
+//Timer
+var timeEl = document.querySelector(".timer");
+
+var secondsLeft = 60;
+
+function setTime() {
+  var timerInterval = setInterval(
+    
+    function() {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
+}
+
+
 startQuiz();
+setTime();
